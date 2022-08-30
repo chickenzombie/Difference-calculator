1b6c2657af99bfd54734ff31e62796f5ac775b4e
Rustem Davletov <chicken.zombie7@gmail.com>
2022-08-30 17:18:53 +0300

    Refactoring

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,7 @@
-const program = require('commander');
+const { Command } = require('commander');
+const program = new Command();
 
 program
   .version('0.1')
   .description('Compares two configuration files and shows a difference')
-  .parse(process.argv);
+  .parse();
