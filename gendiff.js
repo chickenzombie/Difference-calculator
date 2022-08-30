bf149ffd469bf986b4fb941e7cc00524d31b954a
Rustem Davletov <chicken.zombie7@gmail.com>
2022-08-31 00:17:38 +0300

    Add option and argument logic

--- gendiff.js
+++ gendiff.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
 const { Command } = require('commander');
 const program = new Command();
 
-program
-  .version('0.1')
+program.command('gendiff')
+  .version('0.0.1')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .description('Compares two configuration files and shows a difference')
-  .parse();
+  .parse(process.argv);
