56d1a8d67dbe9e75fccdd06ed21944eedf52f2b2
chickenzombie <davletov.rustem@mail.ru>
2022-10-04 22:07:39 +0300

    Remove duplicate code from tests

--- tests/gendiff.test.js
+++ tests/gendiff.test.js
@@ -11,17 +11,17 @@ const expectedStylishFormat = readFileSync(getFixturePath('stylish.txt'), 'utf-8
 const expectedPlainFormat = readFileSync(getFixturePath('plain.txt'), 'utf-8');
 const expectedJSONFormat = readFileSync(getFixturePath('json.txt'), 'utf-8');
 
-test('test stylish formatter', () => {
+test('stylish formatter', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toBe(expectedStylishFormat);
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish')).toBe(expectedStylishFormat);
 });
 
-test('test plain formatter', () => {
+test('plain formatter', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(expectedPlainFormat);
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toBe(expectedPlainFormat);
 });
 
-test('test json formatter', () => {
+test('json formatter', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(expectedJSONFormat);
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toBe(expectedJSONFormat);
 });
